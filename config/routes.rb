8cc9c2cc077b417938c02e0fd1914120c7ae7a90
Noemi J. Hernandez <noemij.hernandez@gmail.com>
2016-10-23 10:47:01 -0700

    Added the info page, nad and footer pages and rspec bundle

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+
   resources :adventures
   root 'adventures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
